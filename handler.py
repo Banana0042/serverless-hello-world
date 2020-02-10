21ce4f63d6441f3ad11eb4a89891aa1c67197273
Robert Walker <banana0042@gmail.com>
2020-02-10 13:06:04 -0700

    attempt 2

--- handler.py
+++ handler.py
@@ -15,11 +15,3 @@ def hello(event, context):
 
     return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Hello World. Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
